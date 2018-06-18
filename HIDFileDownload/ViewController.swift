438c40e42c7271c1aa20c84efe1745f764c99e9c
Maharani SVS <mahavrani@gmail.com>
2018-06-18 15:37:58 +0530

    Initial Commit

---
+++ HIDFileDownload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HIDFileDownload
+//
+//  Created by Salute on 18/06/18.
+//  Copyright © 2018 Maharani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
